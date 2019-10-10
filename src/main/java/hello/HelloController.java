212a33209743466ef96a9837a66ace5b9fa0d321
Maynic Ye <zye03@ucsb.edu>
2019-10-09 22:53:38 -0700

    link repo

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,10 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        String html = "<h1>Hello World!</h1>\n" +
+        "<p>This web app is powered by \n" +
+        "<a href='https://github.com/zye03/spring-boot-minimal-webapp'>this github repo</a></p>\n";
+        return html;
     }
     
 }
